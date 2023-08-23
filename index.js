dc1c40252d7ea91c7b2f562fefc294ba64693989
Daniel Lozano Campano <danlozcam@gmail.com>
2023-08-23 20:19:23 +0200

    finished dotenv exercise

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 // reference: https://www.npmjs.com/package/dotenv
+const fs = require('node:fs')
 
 function config (options = {}) {
-  // your code here
+  let dotenv
+  const path = options.path ?? '.env'
+  try {
+    dotenv = fs.readFileSync(path, 'utf-8')
+  } catch (e) {
+    return
+  }
+  if (!dotenv) return
+
+  const lines = dotenv.split('\n')
+  for (const line of lines) {
+    const [key, value] = line.split('=')
+    process.env[key] = value.split('"').join('')
+  }
 }
 
 module.exports = { config }
